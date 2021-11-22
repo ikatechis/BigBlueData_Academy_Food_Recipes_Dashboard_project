2d85290ae765384aeb94c6d8aeb99066446b96ce
Iasonas Katechis <iasonas.katechis@gmail.com>
2021-11-22 10:57:56 +0200

    finish tab2 layout

--- application/app.py
+++ application/app.py
@@ -12,9 +12,11 @@ import plotly.graph_objects as go
 
 import dash
 from dash import dcc
+import dash_bootstrap_components as dbc
 from dash import html
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 from dash.dependencies import Input, Output, State
+from skimage import io
 
 
 
@@ -48,8 +50,7 @@ health_opt = ['Kid Friendly', 'Toddler Friendly', 'Baby Friendly', 'Sugar Free',
               'Healthy', 'Gluten Free', 'Egg Free', 'Diabetic Friendly', 'Low Cholesterol']
 
 other_opt = ['Light', 'Easy', 'Spicy', 'Served Hot', 'Served Cold', 'Summer', 'Fall', 'Winter', 'Spring', 
-             '< 60 Mins', '< 30 Mins', '< 4 Hours', '< 15 Mins', '> 1 Day', 'Kid Friendly', 'Toddler Friendly', 
-             'Baby Friendly', 'College', 'Comfort Food']
+             '< 60 Mins', '< 30 Mins', '< 4 Hours', '< 15 Mins', '> 1 Day', 'College', 'Comfort Food']
 
 rec = pd.read_csv('../data/cleaned_recipes_fotis.csv')
 
@@ -286,10 +287,96 @@ tab1 = html.Div([
 ], 
 className='tab1')
 
+colors2 = ['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f']
 
-tab2 = html.Div([
-    html.H3('Tab content 2')
-], className='tab2')
+tab2 = html.Div(className='tab2', children=[
+    html.Div(id = 'dropdowns-div', children = [
+
+        html.Div( id='drop-menu', children=[
+            html.Label(['Cuisine'], style={'font-weight': 'bold', "text-align": "center"}),
+            dcc.Dropdown(
+                id='multi_dropdown1',
+                options=[{'label': i, 'value': i} for i in cuisine_opt],
+                value=[],
+                multi=True
+            ),
+            html.Label(['Menu position'], style={'font-weight': 'bold', "text-align": "center"}),
+            dcc.Dropdown(
+                id='multi_dropdown2',
+                options=[{'label': i, 'value': i} for i in menu_opt],
+                value=[],
+                multi=True
+            ),
+            html.Label(['Type'], style={'font-weight': 'bold', "text-align": "center"}),
+            dcc.Dropdown(
+                id='multi_dropdown3',
+                options=[{'label': i, 'value': i} for i in type_opt],
+                value=[],
+                multi=True
+            ),
+            html.Label(['Time of day'], style={'font-weight': 'bold', "text-align": "center"}),
+            dcc.Dropdown(
+                id='multi_dropdown4',
+                options=[{'label': i, 'value': i} for i in time_of_day_opt],
+                value=[],
+                multi=True
+            )],
+        style={'width': '49%', 'display': 'inline-block'}),
+
+        html.Div([
+            html.Label(['Occasion'], style={'font-weight': 'bold', "text-align": "center"}),
+            dcc.Dropdown(
+                id='multi_dropdown5',
+                options=[{'label': i, 'value': i} for i in special_opt],
+                value=[],
+                multi=True
+            ),
+             html.Label(['Health tags'], style={'font-weight': 'bold', "text-align": "center"}),
+            dcc.Dropdown(
+                id='multi_dropdown6',
+                options=[{'label': i, 'value': i} for i in health_opt],
+                value=[],
+                multi=True
+            ),
+            html.Label(['Other tags'], style={'font-weight': 'bold', "text-align": "center"}),
+            dcc.Dropdown(
+                id='multi_dropdown7',
+                options=[{'label': i, 'value': i} for i in other_opt],
+                value=[],
+                multi=True
+            )], 
+        style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
+    ], style={
+        # 'borderBottom': 'thin lightgrey solid',
+        # 'backgroundColor': colors2[4],
+        'padding': '10px 5px'
+    }),
+
+    html.Div(id='top-top', children=[html.Div(id='top-recipes-div', children=[html.Label(['Select from the top ratings (best first)'], 
+                                            style={'font-weight': 'bold', "text-align": "left"}),
+    dcc.Dropdown(
+                id='top',
+                style={'width': '400px', 'display': 'inline-block','vertical-align': 'top'}),
+          html.Img(id='image'),]),
+    dcc.Textarea(
+        id='text1',
+        )]),
+          
+      
+    html.Div(id='graphs-div', children=[
+        html.Div(id='nutrients-div', children = [
+        dcc.Graph(id='graph1')
+    ]),
+    
+    
+    html.Div(id='rec-time-div', children=[
+        dcc.Graph(id='graph2')
+    ])
+    ]),
+    
+
+
+  ])
 
 app = dash.Dash(__name__)
 app.title = 'Recipes Dashboard'
@@ -398,6 +485,111 @@ def update_time(value):
     print(value)
     
     return make_time_figs(value)
+
+@app.callback([
+    dash.dependencies.Output('graph2', 'figure'),
+    dash.dependencies.Output('graph1', 'figure'),
+    dash.dependencies.Output('top', 'options'), 
+    dash.dependencies.Output('top', 'value')],
+    [dash.dependencies.Input('multi_dropdown1', 'value'),
+     dash.dependencies.Input('multi_dropdown2', 'value'),
+     dash.dependencies.Input('multi_dropdown3', 'value'),
+     dash.dependencies.Input('multi_dropdown4', 'value'),
+     dash.dependencies.Input('multi_dropdown5', 'value'),
+     dash.dependencies.Input('multi_dropdown6', 'value'),
+     dash.dependencies.Input('multi_dropdown7', 'value')])
+def text_from_multi_dropdown(md1,md2,md3,md4,md5,md6,md7):
+    mask=pd.Series(np.ones(494949), dtype=bool) # Reset to all True mask
+    if md1 != []:
+        mask *= rec.new_tags.str.contains('|'.join(md1),na=False) #  means 'or'
+    if md2 != []:
+        mask *= rec.new_tags.str.contains('|'.join(md2),na=False)
+    if md3 != []:
+        mask *= rec.new_tags.str.contains('|'.join(md3),na=False)
+    if md4 != []:
+        mask *= rec.new_tags.str.contains('|'.join(md4),na=False)
+    if md5 != []:
+        mask *= rec.new_tags.str.contains('|'.join(md5),na=False)
+    if md6 != []:
+        mask *= np.all([rec.new_tags.str.contains(i,na=False) for i in md6], axis=0) #  means 'and'
+    if md7 != []:
+        mask *= np.all([rec.new_tags.str.contains(i,na=False) for i in md7], axis=0)
+    
+    # barplot nutritional values mean percentage of recommended daily intake per serving
+    d={'Calories':round(rec[mask].calories_daily_perc.mean(),1),
+       'Fat':round(rec[mask].fat_daily_perc.mean(),1),
+       'Saturated Fat':round(rec[mask].satfat_daily_perc.mean(),1),
+       'Cholesterol':round(rec[mask].chol_daily_perc.mean(),1),
+       'Sodium':round(rec[mask].sodium_daily_perc.mean(),1),
+       'Carbohydrate':round(rec[mask].carbon_daily_perc.mean(),1),
+       'Fiber':round(rec[mask].fiber_daily_perc.mean(),1),
+       'Sugar':round(rec[mask].sugar_daily_perc.mean(),1),
+       'Protein':round(rec[mask].protein_daily_perc.mean(),1)}
+    data=pd.Series(d)
+    fig1 = px.bar(data, 
+                 labels={"value": "RDA (%)",'index':''},
+                 color_discrete_sequence=[[colors2[2],colors2[1],colors2[11],
+                                      colors2[3],colors2[4],colors2[5],
+                                      colors2[6],colors2[7],colors2[8]]],
+                 hover_data={'variable':False},
+                 title="Average percentage of recommended daily intake per serving")
+
+    fig1.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)', 'paper_bgcolor': 'rgba(0, 0, 0, 0)'},
+        showlegend=False, )
+
+    # barplot recipe counts per year published % of total recipes publiced that year
+    mask_counts_per_year = rec[mask].groupby('YearPublished').count()
+    mask_counts_per_year.reset_index(inplace=True)
+    b = mask_counts_per_year[['YearPublished','id']]
+    b = b.fillna(0)
+    b.reset_index(inplace=True)
+    count_perc_per_year = a.merge(b , on='YearPublished')
+    count_perc_per_year['perc']=round(100*count_perc_per_year.id_y/count_perc_per_year.id_x, 2)
+
+    fig2 = px.bar(count_perc_per_year, x='YearPublished', y='perc',
+                 labels={"perc": "%",'YearPublished':'Year'},
+                 color_discrete_sequence=[colors2[4]],
+                 title="Filtered recipe counts per year published as % of total recipes publiced that year")
+    fig2.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)', 'paper_bgcolor': 'rgba(0, 0, 0, 0)'},
+         showlegend=False, )
+    
+    top20 = list(rec[mask].sort_values(by='normalized_rating', ascending=False).id_name.head(100))
+    
+
+    return fig2, fig1, [{'label': i, 'value': i} for i in top20], top20[0]
+
+@app.callback([
+    dash.dependencies.Output('image', 'src'),
+    dash.dependencies.Output('text1', 'value')],
+    [dash.dependencies.Input('top', 'value')])
+def select_1(name):
+    default_img_url = 'https://dcassetcdn.com/design_img/10150/25224/25224_294121_10150_image.jpg'
+    if name!=[]:
+        result = np.where(rec.id_name==name)
+        img_url = rec.loc[result[0][0]].first_image_url
+        if img_url=='':
+            img_url = default_img_url
+    else:
+        img_url = default_img_url
+
+    st=''
+    text_ingr=''
+    text_instr=''
+    if name!=[]:
+        s = rec.loc[result[0][0]].ingredients_raw_str
+        for i in range(10):
+            s = s.replace('  ',' ')
+        st=rec.loc[result[0][0]].Name +'\n\nIngredients\n\n'
+        for i in range(len(s.split('"')[1::2])):
+            st += '* ' + s.split('"')[1::2][i].strip()+'\n'
+        text_ingr = st
+        sa = rec.loc[result[0][0]].RecipeInstructions
+        sb=''
+        for i in range(len(sa.split('"')[1::2])):
+            sb += str(i + 1) + '. ' + sa.split('"')[1::2][i].strip()+' '+'\n'
+        text_instr = sb
+    
+    return img_url, text_ingr + '\n' +  text_instr
   
 
 
